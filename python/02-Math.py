3b6c1955539ba98bcc5d9fdff9ef89250fb13ffc
tibor.radacovsky@gmail.com <TbrRdcvsk01>
2017-04-28 16:02:11 +0200

    Basic Math python script.

---
+++ python/02-Math.py
@@ -0,0 +1,24 @@
+# very basic program
+print ("Basic math actions");
+
+x1 = 7
+x2 = 2
+
+x3 = 7.0
+x4 = 2.0
+
+print (x1+x2)
+print (x3+x4)
+
+print (x1-x2)
+print (x3-x4)
+
+print (x1*x2)
+print (x3*x4)
+
+print (x1/x2)
+print (x3/x4)
+
+print (x1%x2)
+print (x3%x4)
+
