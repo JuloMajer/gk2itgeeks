863b6f731309d2fb2efa544e20420bb0f64a8fb6
sakonn <jakub@korymont.eu>
2017-05-12 17:28:44 +0200

    update commit to comment

--- jakub/stars.py
+++ jakub/stars.py
@@ -1,10 +1,25 @@
-count = 0
-while (count < 500 ):
-	print("*")
-	count += 1
+rows = int(raw_input("How many rows do you want to have?"))
+col = int(raw_input("How many * in row?"))
+for i in range(rows):
+     print("*"*col)
+
+
+
+#while (count <  ):
+#	print("+")
+#	count += 1
+#print("*"*i)
+
+
+
 
 # Review comment: TiborR 12-May-2017
 # ahoj, skusal si nieco pozriet aj na webe?
 # napr.: print ("*"*500)
 # see link: http://stackoverflow.com/questions/6293421/python-printing-multiple-times
 # End of review comment
+
+# Answer of the author: Jakub 12-May-2017
+# Dobry, dakujem za upozornenie, internet som iba trosku pozrel ale nenapadlo mi hladat nieco taketo
+# Trosku som to teda upgradol :) :) 
+# Jakub
