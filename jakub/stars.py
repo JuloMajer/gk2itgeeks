683a00d158bd02e67af90d70881175a1625f3562
tiborradacovsky <tibor.radacovsky@gmail.com>
2017-05-12 11:45:23 +0200

    Minor comment

--- jakub/stars.py
+++ jakub/stars.py
@@ -2,3 +2,9 @@ count = 0
 while (count < 500 ):
 	print("*")
 	count += 1
+
+# Review comment: TiborR 12-May-2017
+# ahoj, skusal si nieco pozriet aj na webe?
+# napr.: print ("*"*500)
+# see link: http://stackoverflow.com/questions/6293421/python-printing-multiple-times
+# End of review comment
